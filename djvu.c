74dc505860ee46eb332ed24b9c33c22c57afe57f
Moritz Lipp <mlq@pwmt.org>
2011-10-10 16:53:32 +0200

    Some style updates

--- djvu.c
+++ djvu.c
@@ -14,7 +14,7 @@ plugin_register(zathura_document_plugin_t* plugin)
 bool
 djvu_document_open(zathura_document_t* document)
 {
-  if (!document) {
+  if (document == NULL) {
     goto error_out;
   }
 
@@ -33,7 +33,7 @@ djvu_document_open(zathura_document_t* document)
   document->functions.page_free                 = djvu_page_free;
 
   document->data = malloc(sizeof(djvu_document_t));
-  if (!document->data) {
+  if (document->data == NULL) {
     goto error_out;
   }
 
@@ -45,7 +45,7 @@ djvu_document_open(zathura_document_t* document)
   /* setup format */
   djvu_document->format = ddjvu_format_create(DDJVU_FORMAT_RGB24, 0, NULL);
 
-  if (!djvu_document->format) {
+  if (djvu_document->format == NULL) {
     goto error_free;
   }
 
@@ -54,14 +54,19 @@ djvu_document_open(zathura_document_t* document)
   /* setup context */
   djvu_document->context = ddjvu_context_create("zathura");
 
-  if (!djvu_document->context) {
+  if (djvu_document->context == NULL) {
     goto error_free;
   }
 
   /* setup document */
-  djvu_document->document = ddjvu_document_create_by_filename(djvu_document->context, document->file_path, FALSE);
+  djvu_document->document =
+    ddjvu_document_create_by_filename(
+        djvu_document->context,
+        document->file_path,
+        FALSE
+    );
 
-  if (!djvu_document->document) {
+  if (djvu_document->document == NULL) {
     goto error_free;
   }
 
@@ -69,7 +74,8 @@ djvu_document_open(zathura_document_t* document)
   ddjvu_message_t* msg;
   ddjvu_message_wait(djvu_document->context);
 
-  while ((msg = ddjvu_message_peek(djvu_document->context)) && (msg->m_any.tag != DDJVU_DOCINFO)) {
+  while ((msg = ddjvu_message_peek(djvu_document->context)) &&
+         (msg->m_any.tag != DDJVU_DOCINFO)) {
     if (msg->m_any.tag == DDJVU_ERROR) {
       goto error_free;
     }
@@ -82,17 +88,18 @@ djvu_document_open(zathura_document_t* document)
     goto error_free;
   }
 
-  document->number_of_pages = ddjvu_document_get_pagenum(djvu_document->document);
+  document->number_of_pages =
+    ddjvu_document_get_pagenum(djvu_document->document);
 
   return true;
 
 error_free:
 
-  if (djvu_document->format) {
+  if (djvu_document->format != NULL) {
     ddjvu_format_release(djvu_document->format);
   }
 
-  if (djvu_document->context) {
+  if (djvu_document->context != NULL) {
     ddjvu_context_release(djvu_document->context);
   }
 
@@ -107,11 +114,11 @@ error_out:
 bool
 djvu_document_free(zathura_document_t* document)
 {
-  if (!document) {
+  if (document == NULL) {
     return false;
   }
 
-  if (document->data) {
+  if (document->dat != NULLa) {
     djvu_document_t* djvu_document = (djvu_document_t*) document->data;
     ddjvu_context_release(djvu_document->context);
     ddjvu_document_release(djvu_document->document);
@@ -131,14 +138,14 @@ djvu_document_index_generate(zathura_document_t* document)
 bool
 djvu_document_save_as(zathura_document_t* document, const char* path)
 {
-  if (!document || !document->data || !path) {
+  if (document == NULL || document->data == NULL || path == NULL) {
     return false;
   }
 
   djvu_document_t* djvu_document = (djvu_document_t*) document->data;
 
   FILE* fp = fopen(path, "w");
-  if (!fp) {
+  if (fp == NULL) {
     return false;
   }
 
@@ -157,14 +164,14 @@ djvu_document_attachments_get(zathura_document_t* document)
 zathura_page_t*
 djvu_page_get(zathura_document_t* document, unsigned int page)
 {
-  if (!document || !document->data) {
+  if (document == NULL || document->data == NULL) {
     return NULL;
   }
 
-  djvu_document_t* djvu_document  = (djvu_document_t*) document->data;
-  zathura_page_t* document_page = malloc(sizeof(zathura_page_t));
+  djvu_document_t* djvu_document = (djvu_document_t*) document->data;
+  zathura_page_t* document_page  = malloc(sizeof(zathura_page_t));
 
-  if (!document_page) {
+  if (document_page == NULL) {
     return NULL;
   }
 
@@ -174,7 +181,8 @@ djvu_page_get(zathura_document_t* document, unsigned int page)
   ddjvu_status_t status;
   ddjvu_pageinfo_t page_info;
 
-  while ((status = ddjvu_document_get_pageinfo(djvu_document->document, page, &page_info)) < DDJVU_JOB_OK);
+  while ((status = ddjvu_document_get_pageinfo(djvu_document->document, page,
+          &page_info)) < DDJVU_JOB_OK);
 
   if (status >= DDJVU_JOB_FAILED) {
     free(document_page);
@@ -190,7 +198,7 @@ djvu_page_get(zathura_document_t* document, unsigned int page)
 bool
 djvu_page_free(zathura_page_t* page)
 {
-  if (!page) {
+  if (page == NULL) {
     return false;
   }
 
@@ -221,7 +229,7 @@ djvu_page_form_fields_get(zathura_page_t* page)
 bool
 djvu_page_render_cairo(zathura_page_t* page, cairo_t* cairo)
 {
-  if (!page || !page->document || !cairo) {
+  if (page == NULL || page->document == NULL || cairo == NULL) {
     return false;
   }
 
@@ -229,21 +237,23 @@ djvu_page_render_cairo(zathura_page_t* page, cairo_t* cairo)
   djvu_document_t* djvu_document = (djvu_document_t*) page->document->data;
   ddjvu_page_t* djvu_page        = ddjvu_page_create_by_pageno(djvu_document->document, page->number);
 
-  if (!djvu_page) {
+  if (djvu_page == NULL) {
     return false;
   }
 
   while (!ddjvu_page_decoding_done(djvu_page));
 
-  unsigned int page_width = 0;
+  unsigned int page_width  = 0;
   unsigned int page_height = 0;
+
   if ((page->document->rotate % 180) == 0) {
-    page_width = page->document->scale * page->width;
+    page_width  = page->document->scale * page->width;
     page_height = page->document->scale * page->height;
   } else {
-    page_width = page->document->scale * page->width;
-    page_height = page->document->scale * page->height;
+    page_width  = page->document->scale * page->height;
+    page_height = page->document->scale * page->width;
   }
+
   ddjvu_rect_t rrect = { 0, 0, page_width, page_height };
   ddjvu_rect_t prect = { 0, 0, page_width, page_height };
 
@@ -251,11 +261,23 @@ djvu_page_render_cairo(zathura_page_t* page, cairo_t* cairo)
   ddjvu_page_set_rotation(djvu_page, page->document->rotate / 90);
 
   cairo_surface_t* surface = cairo_get_target(cairo);
-  char* data = (char*)cairo_image_surface_get_data(surface);
+
+  if (surface == NULL) {
+    ddjvu_page_release(djvu_page);
+    return false;
+  }
+
+  char* data = (char*) cairo_image_surface_get_data(surface);
+
+  if (data == NULL) {
+    ddjvu_page_release(djvu_page);
+    return false;
+  }
 
   /* render page */
   ddjvu_page_render(djvu_page, DDJVU_RENDER_COLOR, &prect, &rrect, djvu_document->format,
       cairo_image_surface_get_stride(surface), data);
+
   ddjvu_page_release(djvu_page);
 
   return true;
@@ -265,7 +287,7 @@ djvu_page_render_cairo(zathura_page_t* page, cairo_t* cairo)
 zathura_image_buffer_t*
 djvu_page_render(zathura_page_t* page)
 {
-  if (!page || !page->document) {
+  if (page == NULL || page->document == NULL) {
     return NULL;
   }
 
@@ -273,15 +295,16 @@ djvu_page_render(zathura_page_t* page)
   unsigned int page_width  = page->document->scale * page->width;
   unsigned int page_height = page->document->scale * page->height;
 
-  if (!page_width || !page_height) {
+  if (page_width == 0 || page_height == 0) {
     goto error_out;
   }
 
   /* init ddjvu render data */
   djvu_document_t* djvu_document = (djvu_document_t*) page->document->data;
-  ddjvu_page_t* djvu_page        = ddjvu_page_create_by_pageno(djvu_document->document, page->number);
+  ddjvu_page_t* djvu_page        = ddjvu_page_create_by_pageno(
+      djvu_document->document, page->number);
 
-  if (!djvu_page) {
+  if (djvu_page == NULL) {
     goto error_out;
   }
 
@@ -290,7 +313,8 @@ djvu_page_render(zathura_page_t* page)
   ddjvu_rect_t rrect = { 0, 0, page_width, page_height };
   ddjvu_rect_t prect = { 0, 0, page_width, page_height };
 
-  zathura_image_buffer_t* image_buffer = zathura_image_buffer_create(page_width, page_height);
+  zathura_image_buffer_t* image_buffer =
+    zathura_image_buffer_create(page_width, page_height);
 
   if (image_buffer == NULL) {
     goto error_free;
@@ -300,8 +324,8 @@ djvu_page_render(zathura_page_t* page)
   ddjvu_page_set_rotation(djvu_page, DDJVU_ROTATE_0);
 
   /* render page */
-  ddjvu_page_render(djvu_page, DDJVU_RENDER_COLOR, &prect, &rrect, djvu_document->format,
-      3 * page_width, (char*) image_buffer->data);
+  ddjvu_page_render(djvu_page, DDJVU_RENDER_COLOR, &prect, &rrect,
+      djvu_document->format, 3 * page_width, (char*) image_buffer->data);
 
   return image_buffer;
 
